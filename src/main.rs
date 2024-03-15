0983dff84e4686c25dae75011607324ccaf8e3c2
Muluneh Derese <muluneh.derese@protonmail.com>
2024-03-15 17:00:39 +0300

    feat: optimize docker file and configuration

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,12 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(&configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
