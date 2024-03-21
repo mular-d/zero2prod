8a5c6ac74f8492a04d59bc8a78321c3718f635d5
Muluneh Derese <muluneh.derese@protonmail.com>
2024-03-21 11:22:08 +0300

    feat: add name and email validation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -42,6 +42,35 @@ async fn health_check_works() {
 }
 
 #[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 200 ok when the payload was {}.",
+            description
+        )
+    }
+}
+
+#[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
